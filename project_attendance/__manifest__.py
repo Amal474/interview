b21085b8295b459745d4f714a64ebdb47fc17e89
Amal474 <mohammedamal.n@gmail.com>
2023-12-21 17:38:42 +0300

    description added

--- project_attendance/__manifest__.py
+++ project_attendance/__manifest__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Project Task in Attendance',
-    'version': '17.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Human Resources',
     'license': 'OPL-1',
     'summary': """
         Select project and task while marking attendance
+    """,
+    'description': """
+        - Added 3 new fields project, task and description to attendance model
+        - When user mark their attendance, they should select the related project and task
     """,
     'depends': [
         'base',
